cb51f0213307c2b15b2b9e435b0c7071c2866636
Gleb Lykov <glebdom@gmail.com>
2023-05-23 12:57:00 +0300

    lesson 15

---
+++ lesson_15/src/main/java/StringListIndexOutOfBoundsException.java
@@ -0,0 +1,9 @@
+public class StringListIndexOutOfBoundsException extends RuntimeException {
+    public StringListIndexOutOfBoundsException() {
+        super();
+    }
+
+    public StringListIndexOutOfBoundsException(String msg) {
+        super(msg);
+    }
+}
